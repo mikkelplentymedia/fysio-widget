04b94217b0a4bdaf7f1fc879712b7289ff1c0b55
mikkelplentymedia <mikkel@plentymedia.dk>
2025-03-22 16:49:40 +0100

    Update getHoldData.js

--- netlify/functions/getHoldData.js
+++ netlify/functions/getHoldData.js
@@ -3,32 +3,37 @@ export default async (req, context) => {
   const slug_id = query.slug_id;
 
   if (!slug_id) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: "slug_id mangler i URL'en 🙃" }),
-    };
+    return new Response(
+      JSON.stringify({ error: "slug_id mangler i URL'en 🙃" }),
+      {
+        status: 400,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
   }
 
   try {
-    const response = await fetch(`https://hook.eu2.make.com/t2sx95vvn9guk0wvlopopzrafclcnexu?slug_id=${slug_id}`, {
-      method: 'GET',
-    });
-
+    const response = await fetch(`https://hook.eu2.make.com/t2sx95vvn9guk0wvlopopzrafclcnexu?slug_id=${slug_id}`);
     const data = await response.json();
 
-    return {
-      statusCode: 200,
+    return new Response(JSON.stringify(data), {
+      status: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data),
-    };
-
+    });
   } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Noget gik galt 😵' }),
-    };
+    return new Response(
+      JSON.stringify({ error: "Noget gik galt 😵" }),
+      {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
   }
 };
